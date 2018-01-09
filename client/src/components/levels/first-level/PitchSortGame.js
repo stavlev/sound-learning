6b49e58bf39ffeb2076305001fca62af95469c97
stavl <stavlev95@gmail.com>
2018-01-09 22:22:48 +0200

    feat(add-react-sortable-hoc): Added react-sortable-hoc to present the PitchComponents in a sorted row

--- client/src/components/levels/first-level/PitchSortGame.js
+++ client/src/components/levels/first-level/PitchSortGame.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import PitchComponent from "./PitchComponent";
+import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
+
+const SortableItem = SortableElement(({value}) =>
+    <li>
+        <PitchComponent frequency={value}/>
+    </li>
+);
+
+const SortableList = SortableContainer(({items}) => {
+    return (
+        <ul className="pitch-component-list">
+            {items.map((value, index) => (
+                <SortableItem key={`item-${index}`} index={index} value={value}/>
+            ))}
+        </ul>
+    );
+});
 
 export default class PitchSortGame extends React.Component {
+    state = {
+        frequencies: [440, 540, 640, 740, 840],
+    };
+
+    onSortEnd = ({oldIndex, newIndex}) => {
+        this.setState({
+            frequencies: arrayMove(this.state.frequencies, oldIndex, newIndex),
+        });
+    };
+
     render() {
         return (
             <div className="pitch-sort-game">
-                <PitchComponent frequency={440}/>
+                <SortableList items={this.state.frequencies} onSortEnd={this.onSortEnd}/>
             </div>
         )
     }

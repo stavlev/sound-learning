b220a4c8bdf32510f146f901a132fe08ff5c892c
TomerPeer <tomerpeer2002@gmail.com>
2018-05-27 21:09:54 +0300

    Tomer fixed next level problem

---
+++ client/src/constants/levelPositions.js
@@ -0,0 +1,10 @@
+export const PITCH_LEVELS = 1;
+export const LOUDNESS_LEVELS = 2;
+export const CONVOLUTION_LEVELS = 3;
+export const WAVE_LENGTH_LEVELS = 4;
+export const WAVE_SHAPE_LEVELS = 5;
+
+export const PITCH_SORT_GAME = 2;
+export const PITCH_MULTI_CHOICE_GAME = 3;
+export const PITCH_MEMORY_GAME = 4;
+export const PITCH_EQUALIZER = 5;

943c813b38d4ce77570928432f5f53a05b6d9147
stavl <stavlev95@gmail.com>
2018-05-27 19:58:03 +0300

    Added constants.js as src/constants/simonSaysParams.js

---
+++ client/src/constants/simonSaysParams.js
@@ -0,0 +1,19 @@
+export const COLORS = {
+    red: '#FF4136',
+    blue: '#0074D9',
+    green: '#2ECC40',
+    yellow: '#FFDC00',
+    white: 'white',
+    dark: '#1B1A1A',
+}
+
+export const IDS = [
+    'green',
+    'red',
+    'yellow',
+    'blue',
+];
+
+export const SONG_DELAY_TIME = 200;
+export const REDUCED_DELAY_TIME = SONG_DELAY_TIME - 100;
+export const NEXT_LEVEL_DELAY_TIME = 500;
